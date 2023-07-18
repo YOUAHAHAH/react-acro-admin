459c6dc4b9bcdfeadd31d0566600352212119963
YOUAH <101163308+YOUAHAHAH@users.noreply.github.com>
2023-07-18 10:59:13 +0800

    待完善

--- src/router/utils/authRouter.tsx
+++ src/router/utils/authRouter.tsx
@@ -22,7 +22,7 @@ const authRouter = (props: { children: JSX.Element; auth: AuthState }) => {
 
   axiosCanceler.removeAllPending(); // 在跳转路由之前，清除所有的请求
 
-  if (!route.meta?.requiresAuth) return children; // 判断当前路由是否需要访问权限(不需要权限直接放行)
+  // if (!route.meta?.requiresAuth) return children; // 判断当前路由是否需要访问权限(不需要权限直接放行)
 
   if (!token) return <Navigate to="/login" replace />; // 判断是否登录
 
